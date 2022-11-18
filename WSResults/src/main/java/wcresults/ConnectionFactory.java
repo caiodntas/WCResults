a0c4a54fca686cc5cf066418ea60e9641853d13b
L1NEA <alinewacerqueira@gmail.com>
2022-11-18 20:04:04 -0300

    Update ConnectionFactory.java

--- WSResults/src/main/java/wcresults/ConnectionFactory.java
+++ WSResults/src/main/java/wcresults/ConnectionFactory.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package wcresults;
 
-/**
- *
- * @author 822161400
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionFactory {
-    
+    private String usuario = "root";
+    private String senha = "anima123";
+    private String host = "localhost";
+    private String porta = "3306";
+    private String bd = "wc_results";
+
+    public Connection obtemConexao (){
+        try{
+            Connection c = DriverManager.getConnection("jdbc:mysql://" + host + ":" + porta + "/" + bd, usuario, senha);
+            return c;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
