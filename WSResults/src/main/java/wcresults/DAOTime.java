a004db64918f3d1db84fa0ed44aa43f30ef5d01e
L1NEA <alinewacerqueira@gmail.com>
2022-11-20 21:26:26 -0300

    a

--- WSResults/src/main/java/wcresults/DAOTime.java
+++ WSResults/src/main/java/wcresults/DAOTime.java
@@ -31,6 +31,17 @@ public class DAOTime {
         for (time = 0; time < 32; time++)
         update time set.grupo    
     }
+    
+    public void atribuirTimesOficiais(Time time) throws Exception {
+        String sql = "UPDATE time_table SET nome = 'Qatar' AND grupo = 'A' WHERE id = 1 ";
+        try (Connection c = ConnectionFactory.obtemConexao();
+            PreparedStatement ps = c.prepareStatement(sql)) {
+            ps.setString(1, time.getNome());
+            ps.setString(2, time.getGrupo());
+            ps.execute();
+            ps.close();
+            }
+    }
 }    
 
 
